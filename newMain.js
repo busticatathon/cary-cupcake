378f0c9c43456587e5c0cbf6bcd5147a3fbfdfe2
Keegan Lanzillotta <swfanboy01@gmail.com>
2018-09-08 14:06:43 -0400

    Initial Commit

---
+++ newMain.js
@@ -0,0 +1,16 @@
+//obfuscation
+//false = true;
+//true = !false;
+//undefined = "false";
+
+
+//function true() = !false;
+
+
+
+var n3w = new Image();
+n3w.onload = function(){
+	document.getElementById("widecupcake").src = this.src;
+};
+n3w.src = "bake-baked-bakery.jpg"
+
